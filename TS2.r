2985398ab147d4dd5a2c984ef6b06c37deac13bc
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2022-06-03 09:53:01 +0200

    Update TS2.r

--- TS2.r
+++ TS2.r
@@ -5,58 +5,58 @@ library(janitor)
 # DATA ----
 
 ## raw data ----
-TS2_raw <- "Tier,Rarity,Power,Name,Capmax
-2,violet,steam,ATSF 3000,45
-3,silver,steam,CLASS A-1 BERKSHIRE,20
-1,gold,steam,CRAMPTON,60
-4,silver,steam,C&O T-1,20
-2,silver,diesel,DB-BAUREIHE V 100,20
-2,violet,diesel,DB-BAUREIHE V 200,45
-3,blue,steam,DR-BAUREIHE 44,30
-3,silver,steam,DR-BAUREIHE 86,20
-3,silver,electric,DR-BAUREIHE E 60,20
-3,violet,electric,DR-BAUREIHE E 93,45
-2,blue,steam,DR 18 201,30
-4,silver,diesel,DRG V 140 001,20
-2,violet,electric,EASTER MIREO,60
-2,gold,electric,EP-2 BIPOLAR,60
-3,blue,steam,ERIE K-5A,30
-1,gold,steam,ERIEL L-1,60
-3,blue,electric,FLYEUROPE CARAVAGGIO,40
-2,violet,steam,FS CLASS 670,45
-1,silver,steam,FS CLASS 740,20
-1,silver,steam,GER CLASS S69,20
-1,violet,steam,GWR 3041 THE QUEEN,45
-3,blue,steam,GWR 6000 CLASS KING,30
-3,blue,steam,GWR CITY OF TRURO,30
-3,gold,steam,GWRT OLTON HALL,60
-2,violet,electric,KROKODIL CE 6/8,45
-1,blue,steam,LB&SCR B4,30
-3,violet,steam,LMS HUGHES CRAB,45
-1,gold,steam,LNER A4 MALLARD,60
-2,silver,steam,LNER K3,20
-2,blue,diesel,LRZ 14,30
-4,silver,steam,MILWAUKEE ROAD, 20
-2,blue,electric,MILWAUKEE ROAD EF-1,30
-3,silver,steam,NEW YORK CENTRAL HUDSON,20
-1,blue,steam,NORD 140,30
-3,blue,electric,NSB EL 4,30
-1,silver,steam,PREUẞISCHE P8,20
-2,blue,steam,PREUẞISCHE T 14,30
-2,blue,steam,PRR K-4,30
-4,silver,electric,SAR CLASS 8E,30
-3,violet,electric,SBB AE 4/7,45
-2,silver,steam,SECR N CLASS,20
-1,violet,steam,SHAY CLASS C,45
-1,silver,steam,STAR CLASS 4000,20
-3,violet,diesel,SŽD EMCH 3,45
-3,gold,electric,TFR CLASS 19E,80
-3,violet,steam,UP CLASS 9000,45
-3,violet,diesel,UP GTEL 3RD GEN,45
-3,violet,diesel,UP GTEL VERANDA,45
-4,blue,steam,UP FEF-3,30
-3,silver,steam,USRA HEAVY PACIFIC,20
-2,silver,steam,VICTORIAN C CLASS,20
+TS2_raw <- "Name,Tier,Rarity,Power,Capmax
+ATSF 3000,2,violet,steam,45
+CLASS A-1 BERKSHIRE,3,silver,steam,20
+CRAMPTON,1,gold,steam,60
+C&O T-1,4,silver,steam,20
+DB-BAUREIHE V 100,2,silver,diesel,20
+DB-BAUREIHE V 200,2,violet,diesel,45
+DR-BAUREIHE 44,3,blue,steam,30
+DR-BAUREIHE 86,3,silver,steam,20
+DR-BAUREIHE E 60,3,silver,electric,20
+DR-BAUREIHE E 93,3,violet,electric,45
+DR 18 201,2,blue,steam,30
+DRG V 140 001,4,silver,diesel,20
+EASTER MIREO,2,violet,electric,60
+EP-2 BIPOLAR,2,gold,electric,60
+ERIE K-5A,3,blue,steam,30
+ERIEL L-1,1,gold,steam,60
+FLYEUROPE CARAVAGGIO,3,blue,electric,40
+FS CLASS 670,2,violet,steam,45
+FS CLASS 740,1,silver,steam,20
+GER CLASS S69,1,silver,steam,20
+GWR 3041 THE QUEEN,1,violet,steam,45
+GWR 6000 CLASS KING,3,blue,steam,30
+GWR CITY OF TRURO,3,blue,steam,30
+GWRT OLTON HALL,3,gold,steam,60
+KROKODIL CE 6/8,2,violet,electric,45
+LB&SCR B4,1,blue,steam,30
+LMS HUGHES CRAB,3,violet,steam,45
+LNER A4 MALLARD,1,gold,steam,60
+LNER K3,2,silver,steam,20
+LRZ 14,2,blue,diesel,30
+MILWAUKEE ROAD,4,silver,steam,20
+MILWAUKEE ROAD EF-1,2,blue,electric,30
+NEW YORK CENTRAL HUDSON,3,silver,steam,20
+NORD 140,1,blue,steam,30
+NSB EL 4,3,blue,electric,30
+PREUẞISCHE P8,1,silver,steam,20
+PREUẞISCHE T 14,2,blue,steam,30
+PRR K-4,2,blue,steam,30
+SAR CLASS 8E,4,silver,electric,30
+SBB AE 4/7,3,violet,electric,45
+SECR N CLASS,2,silver,steam,20
+SHAY CLASS C,1,violet,steam,45
+STAR CLASS 4000,1,silver,steam,20
+SŽD EMCH 3,3,violet,diesel,45
+TFR CLASS 19E,3,gold,electric,80
+UP CLASS 9000,3,violet,steam,45
+UP GTEL 3RD GEN,3,violet,diesel,45
+UP GTEL VERANDA,3,violet,diesel,45
+UP FEF-3,4,blue,steam,30
+USRA HEAVY PACIFIC,3,silver,steam,20
+VICTORIAN C CLASS,2,silver,steam,20
 "
 ## factor definitions ----
 fcts <- list(con = c(BRITAIN = 1L, GERMANY = 2L, USA = 3L, FRANCE = 4L),
@@ -65,7 +65,7 @@ fcts <- list(con = c(BRITAIN = 1L, GERMANY = 2L, USA = 3L, FRANCE = 4L),
              pow = c(STEAM = "steam", DIESEL = "diesel", ELECTRIC = "electric"))
 
 # TIDY ----
-TS2 <- read_delim(TS2_raw, col_types = "iffcd", lazy = FALSE) %>% 
+TS2 <- read_delim(TS2_raw, col_types = "ciffd", lazy = FALSE) %>% 
   mutate(Country = factor(Tier, levels = fcts$con, labels = names(fcts$con)),
          Rating = factor(Rarity, levels = fcts$rat, labels = names(fcts$rat)),
          Rarity = factor(Rarity, levels = fcts$rar, labels = names(fcts$rar)),
